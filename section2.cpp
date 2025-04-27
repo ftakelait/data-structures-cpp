d0053672283c01c9c1f54e22198e37c968a8a143
Fouzi Takelait <ftakelait@gmail.com>
2025-04-27 00:28:19 -0500

    updated

--- section2.cpp
+++ section2.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+// structure definition
+struct Rectangle
+{
+	int length;  // sizeof(length) is 4 bytes
+	int breadth;  // sizeof(breadth) is 4 bytes 
+};  // total size of this structure is 4*4=8 bytes (it occupies 8 bytes in memory) but not yet untill we declare it in the main() function. 
+
 int main()
 {
 	int A[5] = {4, 3, 5};
@@ -11,5 +18,12 @@ int main()
 		cout << x << endl;
 	}
 	
+	cout << endl;
+			
+	// declaration of the structure and initialization
+	struct Rectangle r = {15, 2}; // now it occupies a scpace in memory
+	
+	// accessing a member of a structure
+	cout << "Area of a rectangle is: " << r.length * r.breadth << endl;
 	return 0;
 }
